fb16a7f5dc1f0418e7030ba6941f88257814d04d
deepanshu1920 <deepanshu1920>
2024-09-05 13:27:02 +0530

    new chan

--- backhand.js
+++ backhand.js
@@ -1,14 +1,52 @@
-const input_box = document.getElementById("input-box");
-const down_list = document.getElementById("pull_down_list");
+const inputBox = document.getElementById("input-box");
+const pullDownList = document.getElementById("pull_down_list");
 function  addTask(){
-    if (input_box.value === ''){
-        alert("Please enter a task");
-    }
-    else{
-        let li = document.createElement("li");
-        li.innerHTML = input_box.value;
-        list.down_list.appendChild(li);
+    // Get the input value
+    const inputValue = inputBox.value;
 
-    }
+    // Create a new li element
+    const newLi = document.createElement('li');
+    newLi.textContent = inputValue;
 
+    // Add the new li element to the ul list
+    pullDownList.appendChild(newLi);
+
+    // Clear the input box
+    inputBox.value = '';
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // if (input_box.value === ''){
+    //     alert("Please enter a task");
+    // }
+    // else{
+    //     let li = document.createElement("li");
+    //     li.innerHTML = input_box.value;
+    //     list.pullDownList.appendChild(li);
+
+    //     let del = document.createElement("del");
+    //     del.innerHTML = "\u007d";
+    //     li.appendChild(del)
+        
+
+    // }
+    // input_box.value="";
+
